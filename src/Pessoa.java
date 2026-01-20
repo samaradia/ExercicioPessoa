2fd60ce9ee87d04501ea9d1f1832c079deef039b
Samara Dias da Silva <marcoscz329@gmail.com>
2026-01-20 18:13:27 -0300

    Atualização no main

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa: " + nome + "(Idade: " + idade + ")";
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+import java.util.ArrayList;
+
+public class Principal {
     public static void main(String[] args) {
+        ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
+
+        Pessoa pessoa1 = new Pessoa("Lilika",18);
+        Pessoa pessoa2 = new Pessoa("Willian", 21);
+        Pessoa pessoa3 = new Pessoa("Samara", 19);
+
+        listaDePessoas.add(pessoa1);
+        listaDePessoas.add(pessoa2);
+        listaDePessoas.add(pessoa3);
+
+        System.out.println("O tamanho da lista: " + listaDePessoas.size());
+        System.out.println("Primeiro da lista: " + listaDePessoas.get(0));
+        System.out.println("Lista completa: ");
+        for (Pessoa pessoa : listaDePessoas) {
+            System.out.println(pessoa);
+        }
+
 
     }
 }
